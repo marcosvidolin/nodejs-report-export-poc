99c984290fc7eb9aa8e9ee1e659db4493d45bec1
Marcos Vidolin <marcosvidolin@gmail.com>
2021-05-02 17:44:29 -0300

    feat: excel

--- README.md
+++ README.md
@@ -2,6 +2,18 @@
 
 A Nodejs Report Export PoC
 
+### To build
+
+```shell
+docker-compose build
+```
+
+## How to run
+
+```shell
+docker-compose up
+```
+
 ## Documentation
 
 https://docs.google.com/drawings/d/15mGsFzDfqhYVwjp0qOe_oAM8u83j0RQExHWMlTaz3Fc/edit
--- src/excel.js
+++ src/excel.js
@@ -3,14 +3,113 @@ const ExcelJS = require('exceljs');
 module.exports = {
   createWorkbook: () => {
     const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet('First');
+    const worksheet = workbook.addWorksheet('Terra Viva');
     worksheet.columns = [
       { header: 'Id', key: 'id', width: 10 },
       { header: 'Name', key: 'name', width: 32 },
-      { header: 'D.O.B.', key: 'DOB', width: 10 }
+      { header: 'E-Mail', key: 'email', width: 10 },
+      { header: 'Active?', key: 'active', width: 10 },
+      { header: 'Created At', key: 'created_at', width: 10 },
     ];
-    worksheet.addRow({id: 1, name: 'John Doe', dob: new Date(1970,1,1)});
-    worksheet.addRow({id: 2, name: 'Jane Doe', dob: new Date(1965,1,7)});
+    worksheet.addRow({id: '1', name: 'Lettie	Meeron', email:	'lmeeron0@ezinearticles.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '2', name: 'Mora	Gallon', email:	'mgallon1@360.cn',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '3', name: 'Alanson	Corner', email:	'acorner2@cnbc.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '4', name: 'Shaylyn	Philippsohn', email:	'sphilippsohn3@msn.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '5', name: 'Bourke	Tellesson', email:	'btellesson4@sohu.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '6', name: 'Rubetta	Van Son', email: 'rvanson5@usda.gov',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '7', name: 'Karil	Bricham', email: 'kbricham6@goo.ne.jp',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '8', name: 'Wit	Digges', email:	'wdigges7@nasa.gov',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '9', name: 'Budd	Prantl', email:	'bprantl8@utexas.edu',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '10', name: 'Konrad	Klementz', email:	'kklementz9@hexun.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '11', name: 'Robenia	Cornels', email:	'rcornelsa@eepurl.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '12', name: 'Gradey	Quigley', email:	'gquigleyb@nps.gov',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '13', name: 'Halsey	Ramalhete', email:	'hramalhetec@angelfire.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '14', name: 'Daryn	Gribble', email:	'dgribbled@reference.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '15', name: 'Erl	Lissandrini' , email: 'elissandrinie@cocolog-nifty.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '16', name: 'Cammy	Secretan', email:	'csecretanf@wufoo.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '17', name: 'Mona	Priest', email:	'mpriestg@newyorker.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '18', name: 'Lenee	Eykelhof', email:	'leykelhofh@wikispaces.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '19', name: 'Inessa	Hugonneau', email:	'ihugonneaui@tumblr.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '20', name: 'Krishna	Morrall', email:	'kmorrallj@loc.gov',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '21', name: 'Boyce	Grose', email:	'bgrosek@stanford.edu',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '22', name: 'Gaye	Nardrup', email:	'gnardrupl@instagram.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '23', name: 'Arluene	Muttock', email:	'amuttockm@quantcast.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '24', name: 'Brew	Frizell', email:	'bfrizelln@lycos.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '25', name: 'Poul	McDuffy', email:	'pmcduffyo@spotify.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '26', name: 'Bernie	Gary', email:	'bgaryp@biblegateway.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '27', name: 'Ludwig	Harbisher', email:	'lharbisherq@edublogs.org',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '28', name: 'Star	Saffell', email:	'ssaffellr@tiny.cc',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '29', name: 'Stuart	Wannell', email:	'swannells@twitpic.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '30', name: 'June	Gipp', email:	'jgippt@cloudflare.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '31', name: 'Denis	Tichner', email:	'dtichneru@blogtalkradio.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '32', name: 'Shaina	Extence', email:	'sextencev@psu.edu',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '33', name: 'Lotte	Tithecote' , email: 'ltithecotew@timesonline.co.uk',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '34', name: 'Ambrosio	Lysaght', email: 'alysaghtx@people.com.cn',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '35', name: 'Carlota	Dunsire', email:	'cdunsirey@narod.ru',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '36', name: 'Cart	Traher', email:	'ctraherz@elegantthemes.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '37', name: 'Randi	Fawke', email:	'rfawke10@mapquest.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '38', name: 'Renato	Bampkin', email:	'rbampkin11@newyorker.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '39', name: 'Rena	Vala', email:	'rvala12@de.vu',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '40', name: 'Sara	Raselles', email:	'sraselles13@weather.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '41', name: 'Gilly	Dight', email:	'gdight14@myspace.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '42', name: 'Sylas	MacInerney', email:	'smacinerney15@spotify.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '43', name: 'Alexine	Rableau', email:	'arableau16@twitter.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '44', name: 'Ursa	Kemitt', email:	'ukemitt17@google.nl',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '45', name: 'Halli	Breckin', email:	'hbreckin18@google.fr',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '46', name: 'Dawn	Gonsalo', email:	'dgonsalo19@alexa.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '47', name: 'Miquela	Tolomei', email:	'mtolomei1a@imgur.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '48', name: 'Cathee	Gott', email:	'cgott1b@economist.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '49', name: 'Cristiano	Lakeman', email:	'clakeman1c@prnewswire.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '50', name: 'Arch	Baldacchino', email:	'abaldacchino1d@paginegialle.it',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '51', name: 'Cristal	Petyakov', email:	'cpetyakov1e@weebly.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '52', name: 'Leta	Eskriet', email:	'leskriet1f@twitpic.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '53', name: 'Tiebold	Mintoff', email:	'tmintoff1g@com.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '54', name: 'Marve	Kendal', email:	'mkendal1h@theguardian.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '55', name: 'Becka	Wilby', email:	'bwilby1i@yandex.ru',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '56', name: 'Gaspar	Cheer', email:	'gcheer1j@ucla.edu',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '57', name: 'Sibyl	Rowlett', email:	'srowlett1k@zimbio.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '58', name: 'Payton	Earwicker', email:	'pearwicker1l@admin.ch',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '59', name: 'Dian	Glasgow', email:	'dglasgow1m@usnews.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '60', name: 'Giuseppe	Hovenden', email:	'ghovenden1n@sogou.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '61', name: 'Misha	Baffin', email:	'mbaffin1o@meetup.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '62', name: 'Prent	Fallon', email: 'pfallon1p@pagesperso-orange.fr',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '63', name: 'Tod	OScanlon', email:	'toscanlon1q@netlog.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '64', name: 'Stesha	Braitling', email:	'sbraitling1r@google.fr',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '65', name: 'Jefferey	Buggy', email:	'jbuggy1s@icio.us',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '66', name: 'Pedro	Touzey', email:	'ptouzey1t@opera.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '67', name: 'Brigida	Coutthart', email:	'bcoutthart1u@gnu.org',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '68', name: 'Waring	OBeirne', email: 'wobeirne1v@berkeley.edu',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '69', name: 'Diane-marie	Nenci', email: 'dnenci1w@opensource.org',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '70', name: 'Ibby	Dugue', email:	'idugue1x@pcworld.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '71', name: 'Mordy	Kembry', email:	'mkembry1y@hexun.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '72', name: 'Christy	Prestie', email:	'cprestie1z@constantcontact.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '73', name: 'Katharyn	Moyer', email:	'kmoyer20@bloglines.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '74', name: 'Rabbi	Gravell', email:	'rgravell21@chronoengine.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '75', name: 'Aindrea	Pluck', email: 'apluck22@cam.ac.uk',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '76', name: 'Dixie	Messom', email:	'dmessom23@cdc.gov',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '77', name: 'Lanna	Andrichak', email:	'landrichak24@topsy.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '78', name: 'Dillon	Fairfull', email:	'dfairfull25@sun.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '79', name: 'Garv	Bandey', email:	'gbandey26@senate.gov',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '80', name: 'Emmery	Crombleholme', email:	'ecrombleholme27@ucsd.edu',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '81', name: 'Robers	Collaton', email:	'rcollaton28@answers.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '82', name: 'Cyndie	Pinnijar', email:	'cpinnijar29@ucoz.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '83', name: 'Royce	Sanbroke', email:	'rsanbroke2a@usgs.gov',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '84', name: 'Raina	Algeo', email:	'ralgeo2b@github.io',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '85', name: 'Keslie	Barthot', email:	'kbarthot2c@storify.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '86', name: 'Minnie	Brundale', email:	'mbrundale2d@dropbox.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '87', name: 'Moore	Guillotin', email:	'mguillotin2e@wp.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '88', name: 'Alene	Snodden', email:	'asnodden2f@liveinternet.ru',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '89', name: 'Marney	Thomazet', email:	'mthomazet2g@economist.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '90', name: 'Lannie	Antonsen', email:	'lantonsen2h@army.mil',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '91', name: 'Luca	Gedling', email:	'lgedling2i@opensource.org',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '92', name: 'Davie	Browse', email: 'dbrowse2j@e-recht24.de',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '93', name: 'Ennis	De Caville', email:	'edecaville2k@ca.gov',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '94', name: 'Josey	Challes', email:	'jchalles2l@photobucket.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '95', name: 'Paolo	Boik', email:	'pboik2m@ycombinator.com',	active: 'false', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '96', name: 'Ynes	Garrie', email:	'ygarrie2n@phpbb.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '97', name: 'Giulio	Defty', email:	'gdefty2o@walmart.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '98', name: 'Pace	Dudny', email:	'pdudny2p@ucoz.com',	active: 'true', created_at: new Date(1970,1,1)});
+    worksheet.addRow({id: '100', name: 'Keefe	Codrington', email:	'kcodrington2r@github.com',	active: 'true', created_at: new Date(1970,1,1)});
 
     return workbook;
   }
